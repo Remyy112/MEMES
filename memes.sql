c6eaef255684360b94be7ca464c90bd6f1576eb4
Remek <113498973+Remyy112@users.noreply.github.com>
2022-09-22 11:58:56 +0200

    Update memes.sql

--- memes.sql
+++ memes.sql
@@ -1,7 +1,11 @@
 SELECT * FROM memes WHERE deleted_at IS NULL
 OFFSET 0 LIMIT 10
 
-INSERT INTO users (1, nazwa, haslo)
-VALUE (1, Adam, Adam123)
+SELECT file, updated_at, title, likes FROM memes
 
-hejo
+ 
+
+SELECT m.id, m.file, m.title, m.likes, m.updated_at as "date", u1_name as "author", u2_name as "admin" FROM memes m
+JOIN users u1  ON m.user_id = u1.id
+JOIN users u2 ON m.admin_id = u2.id
+WHERE m.deleted_at IS NULL OFFSET 0 LIMIT 10
